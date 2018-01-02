5f92e5ec4ec8cb344f8104c91ccb3c78734e5afb
Christopher Dorst <chris@dorst.email>
2018-01-02 01:11:52 -0600

    Add GnuGpl3LicenseCommitter

---
+++ DevOps.VersionControl.Functions.AddAndCommitGnuGpl3License/GnuGpl3LicenseCommitter.cs
@@ -0,0 +1,17 @@
+﻿using DevOps.VersionControl.Structures.GitCommitUser;
+using static DevOps.VersionControl.Functions.AddAndCommitGitChanges.GitChangeAdderAndCommitter;
+using static Metaproject.Cdn.Containers.VersionControl.Helpers.VersionControlContentHelper;
+
+namespace DevOps.VersionControl.Functions.AddAndCommitGnuGpl3License
+{
+    public static class GnuGpl3LicenseCommitter
+    {
+        private const string CommitMessage = "Add GNU GPL 3.0 LICENSE";
+
+        public static void AddAndCommitLicense(string repositoryDirectory, UserInfo? user = null)
+        {
+            SaveGnuGpl3License(repositoryDirectory);
+            AddAndCommitChanges(repositoryDirectory, CommitMessage, user);
+        }
+    }
+}
